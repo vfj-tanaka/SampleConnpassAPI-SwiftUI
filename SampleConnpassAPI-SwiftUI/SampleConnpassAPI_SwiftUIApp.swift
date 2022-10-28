c1b3ad21c7f5801d4eec070537d86c744652eea6
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-28 11:14:29 +0900

    Initial Commit

---
+++ SampleConnpassAPI-SwiftUI/SampleConnpassAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleConnpassAPI_SwiftUIApp.swift
+//  SampleConnpassAPI-SwiftUI
+//
+//  Created by mtanaka on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct SampleConnpassAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
